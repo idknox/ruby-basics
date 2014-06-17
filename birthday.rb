15d46ca87cbcfe8a9baa2bae103ed0096cd4990f
Ian Knox <knoxid@gmail.com>
2014-06-16 19:34:20 -0600

    3 files

--- birthday.rb
+++ birthday.rb
@@ -1,26 +1,30 @@
-require 'date'
+def find_names
 
-puts "What is your name?"
+  puts "Type 'exit' and ENTER to close the program, or enter your name: "
 
-name = gets.chomp.capitalize
+  name = gets.chomp.capitalize
 
-filename = 'birthday_data.csv'
+  filename = 'birthday_data.csv'
 
-txt = File.read filename
+  txt = File.read filename
 
-birthday_info = txt.split(',')
+  birthday_info = txt.split(',')
 
-i = 0
+  i = 0
 
-for each in birthday_info
-  if name == each
-    b = birthday_info[i+1]
+  if birthday_info.include?(name)
+    b = birthday_info[(birthday_info.index(name))+1]
     year = b[0..3]
     age = Time.new.year - year.to_i
     puts name + " " + b + " " + age.to_s
+  elsif name == "Exit"
+    exit()
+  else
+    puts "That name is not on the list"
   end
 
   i += 1
-
+  find_names
 end
 
+find_names
